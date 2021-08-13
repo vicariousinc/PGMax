fbc37b415f27e310f0e0090e16d63e5a14f1895f
Guangyao Zhou <StannisZhou@users.noreply.github.com>
2021-08-13 07:45:26 -0700

    Update `VariableGroup`s to better handle `set_evidence` (#54)
    * Make container keys a property
    
    * Add vars_to_evidence to variable groups
    
    * Get rid of a redundant docstring

--- pgmax/fg/groups.py
+++ pgmax/fg/groups.py
@@ -80,6 +80,16 @@ class VariableGroup:
             "Please subclass the VariableGroup class and override this method"
         )
 
+    def get_vars_to_evidence(self, evidence: Any) -> Dict[nodes.Variable, np.ndarray]:
+        """Function that turns input evidence into a dictionary mapping variables to evidence.
+
+        Returns:
+            a dictionary mapping all possible variables to the corresponding evidence
+        """
+        raise NotImplementedError(
+            "Please subclass the VariableGroup class and override this method"
+        )
+
     @property
     def keys(self) -> Tuple[Any, ...]:
         """Function to return a tuple of all keys in the group.
@@ -183,12 +193,7 @@ class CompositeVariableGroup(VariableGroup):
             a dictionary mapping all possible keys to different variables.
         """
         keys_to_vars = {}
-        if isinstance(self.variable_group_container, Mapping):
-            container_keys = self.variable_group_container.keys()
-        else:
-            container_keys = set(range(len(self.variable_group_container)))
-
-        for container_key in container_keys:
+        for container_key in self.container_keys:
             for variable_group_key in self.variable_group_container[container_key].keys:
                 if isinstance(variable_group_key, tuple):
                     keys_to_vars[
@@ -200,6 +205,35 @@ class CompositeVariableGroup(VariableGroup):
                     ] = self.variable_group_container[container_key][variable_group_key]
 
         return keys_to_vars
+
+    def get_vars_to_evidence(
+        self, evidence: Union[Mapping, Sequence]
+    ) -> Dict[nodes.Variable, np.ndarray]:
+        """Function that turns input evidence into a dictionary mapping variables to evidence.
+
+        Args:
+            evidence: A mapping or a sequence of evidences.
+                The type of evidence should match that of self.variable_group_container
+
+        Returns:
+            a dictionary mapping all possible variables to the corresponding evidence
+        """
+        vars_to_evidence: Dict[nodes.Variable, np.ndarray] = {}
+        for key in self.container_keys:
+            vars_to_evidence.update(
+                self.variable_group_container[key].get_vars_to_evidence(evidence[key])
+            )
+
+        return vars_to_evidence
+
+    @cached_property
+    def container_keys(self) -> Tuple:
+        if isinstance(self.variable_group_container, Mapping):
+            container_keys = tuple(self.variable_group_container.keys())
+        else:
+            container_keys = tuple(range(len(self.variable_group_container)))
+
+        return container_keys
 
 
 @dataclass(frozen=True, eq=False)
@@ -226,6 +260,30 @@ class NDVariableArray(VariableGroup):
             keys_to_vars[key] = nodes.Variable(self.variable_size)
         return keys_to_vars
 
+    def get_vars_to_evidence(
+        self, evidence: np.ndarray
+    ) -> Dict[nodes.Variable, np.ndarray]:
+        """Function that turns input evidence into a dictionary mapping variables to evidence.
+
+        Args:
+            evidence: An array of shape self.shape + (variable_size,)
+                An array containing evidence for all the variables
+
+        Returns:
+            a dictionary mapping all possible variables to the corresponding evidence
+        """
+        expected_shape = self.shape + (self.variable_size,)
+        if not evidence.shape == expected_shape:
+            raise ValueError(
+                f"Input evidence should be an array of shape {expected_shape}. "
+                f"Got {evidence.shape}."
+            )
+
+        vars_to_evidence = {
+            self._keys_to_vars[key]: evidence[key] for key in self._keys_to_vars
+        }
+        return vars_to_evidence
+
 
 @dataclass(frozen=True, eq=False)
 class GenericVariableGroup(VariableGroup):
@@ -250,6 +308,36 @@ class GenericVariableGroup(VariableGroup):
         for key in self.key_tuple:
             keys_to_vars[key] = nodes.Variable(self.variable_size)
         return keys_to_vars
+
+    def get_vars_to_evidence(
+        self, evidence: Mapping[Any, np.ndarray]
+    ) -> Dict[nodes.Variable, np.ndarray]:
+        """Function that turns input evidence into a dictionary mapping variables to evidence.
+
+        Args:
+            evidence: An array of shape self.shape + (variable_size,)
+                An array containing evidence for all the variables
+
+        Returns:
+            a dictionary mapping all possible variables to the corresponding evidence
+        """
+        vars_to_evidence = {}
+        for key in evidence:
+            if key not in self._keys_to_vars:
+                raise ValueError(
+                    f"The evidence is referring to a non-existent variable {key}."
+                )
+
+            if evidence[key].shape != (self._keys_to_vars[key].num_states,):
+                raise ValueError(
+                    f"Variable {key} expect an evidence array of shape "
+                    f"({(self._keys_to_vars[key].num_states,)})."
+                    f"Got {evidence[key].shape}."
+                )
+
+            vars_to_evidence[self._keys_to_vars[key]] = evidence[key]
+
+        return vars_to_evidence
 
 
 @dataclass(frozen=True, eq=False)
