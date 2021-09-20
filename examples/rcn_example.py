49ccbf14fba99f5326d7e065529149508fdcbc93
Che Kun Law <chekun@vicarious.com>
2021-09-20 14:41:32 -0700

    First pass of addressing comments

--- examples/rcn_example.py
+++ examples/rcn_example.py
@@ -8,10 +8,16 @@ from rw_common.utils.io import resolve_path
 from rw_common.utils.pickle import PickleHound
 
 
-def _compute_valid_configs(perturb_radius, pool_size=5):
+def _compute_valid_configs(perturb_radius: int, pool_size: int = 5):
     """
-    Helper function to compute valid configurations given perturb radius.
+    Helper function to compute valid configurations given perturb radius. (WIP)
 
+    Args
+        perturb_radius: Perturb radius of edge factor.
+        pool_size: Pool size of the model.
+
+    Returns
+        Dict
     """
     pass
 
@@ -32,24 +38,30 @@ variable_size = pool_size * pool_size
 generic_variable_group = groups.GenericVariableGroup(
     variable_size=variable_size, key_tuple=tuple(range(num_of_variables))
 )
-fg = graph.FactorGraph(variables=generic_variable_group)
+fg = graph.FactorGraph(variables=dict(pool_vars=generic_variable_group))
 
+valid_configs_dict = None
 
-# From edges, add factors to the graph one by onw
 for edge in static_graph.edges:
-
-    curr_keys = [
-        ("pool_vars", edge[0][0]),
-        ("pool_vars", edge[0][1]),
-    ]
-
     perturb_radius = edge[-1]
-    valid_configs_non_supp = _compute_valid_configs(
+
+    valid_config = _compute_valid_configs(
         perturb_radius=perturb_radius, pool_size=pool_size
     )
 
+    valid_configs_dict.update(valid_config)
+
+valid_configs_size = sum(
+    [len(list_of_valid_nodes) for list_of_valid_nodes in valid_configs_dict.keys()]
+)
+
+# From edges, add factors to the graph one by one.
+for edge in static_graph.edges:
+
+    curr_keys = [edge[0][0], edge[0][1]]
+
     fg.add_factor(
         curr_keys,
-        valid_configs_non_supp,
-        np.zeros(valid_configs_non_supp.shape[0], dtype=float),
+        valid_configs_dict,
+        np.zeros(valid_configs_dict.shape[0], dtype=float),
     )
