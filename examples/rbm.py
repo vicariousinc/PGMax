1baba554b573d3b415b5fc1cec4db799ff4841dc
stannis <stannis@vicarious.com>
2021-10-13 21:20:44 -0700

    Clean up RBM notebook; include data

--- examples/rbm.py
+++ examples/rbm.py
@@ -25,7 +25,8 @@ from tqdm import tqdm
 from pgmax.fg import graph, groups
 
 # %%
-params = joblib.load("/home/stannis/rbm_mnist.joblib")
+# Load parameters
+params = joblib.load("example_data/rbm_mnist.joblib")
 bv = params["bv"]
 bh = params["bh"]
 W = params["W"]
@@ -33,13 +34,12 @@ nv = bv.shape[0]
 nh = bh.shape[0]
 
 # %%
+# Build factor graph
 visible_variables = groups.NDVariableArray(variable_size=2, shape=(nv,))
 hidden_variables = groups.NDVariableArray(variable_size=2, shape=(nh,))
 fg = graph.FactorGraph(
     variables=dict(visible=visible_variables, hidden=hidden_variables),
 )
-
-# %%
 for ii in tqdm(range(nh)):
     for jj in range(nv):
         fg.add_factor(
@@ -49,9 +49,8 @@ for ii in tqdm(range(nh)):
         )
 
 # %%
+# Set evidence
 init_msgs = fg.get_init_msgs()
-
-# %%
 init_msgs.evidence["hidden"] = np.stack(
     [np.zeros_like(bh), bh + np.random.logistic(size=bh.shape)], axis=1
 )
@@ -60,12 +59,12 @@ init_msgs.evidence["visible"] = np.stack(
 )
 
 # %%
+# Run inference and decode
 msgs = fg.run_bp(100, 0.5, init_msgs)
-
-# %%
 map_states = fg.decode_map_states(msgs)
 
 # %%
+# Visualize decodings
 img = np.zeros(bv.shape)
 for ii in range(nv):
     img[ii] = map_states[("visible", ii)]
