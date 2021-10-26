86f467ea6dea7fb25a10731fe4d25fab4024d2be
stannis <stannis@vicarious.com>
2021-10-25 23:02:09 -0700

    New test nodes

--- tests/fg/test_nodes.py
+++ tests/fg/test_nodes.py
@@ -1,61 +1,58 @@
+import re
+
 import numpy as np
 import pytest
 
 from pgmax.fg import nodes
 
 
-def test_enumfactor_configints_error():
-    v = nodes.Variable(3)
-    configs = np.array([[1.0]])
-    log_potentials = np.array([1.0])
+def test_enumeration_factor():
+    variable = nodes.Variable(3)
+    with pytest.raises(ValueError, match="Configurations should be integers. Got"):
+        nodes.EnumerationFactor(
+            variables=(variable,),
+            configs=np.array([[1.0]]),
+            log_potentials=np.array([0.0]),
+        )
 
-    with pytest.raises(ValueError) as verror:
-        nodes.EnumerationFactor(tuple([v]), configs, log_potentials)
+    with pytest.raises(ValueError, match="Potential should be floats. Got"):
+        nodes.EnumerationFactor(
+            variables=(variable,),
+            configs=np.array([[1]]),
+            log_potentials=np.array([0]),
+        )
 
-    assert "Configurations" in str(verror.value)
+    with pytest.raises(ValueError, match="configs should be a 2D array"):
+        nodes.EnumerationFactor(
+            variables=(variable,),
+            configs=np.array([1]),
+            log_potentials=np.array([0.0]),
+        )
 
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            "Number of variables 1 doesn't match given configurations (1, 2)"
+        ),
+    ):
+        nodes.EnumerationFactor(
+            variables=(variable,),
+            configs=np.array([[1, 2]]),
+            log_potentials=np.array([0.0]),
+        )
 
-def test_enumfactor_potentials_error():
-    v = nodes.Variable(3)
-    configs = np.array([[1]], dtype=int)
-    log_potentials = np.array([1], dtype=int)
+    with pytest.raises(
+        ValueError, match=re.escape("Expected log potentials of shape (1,)")
+    ):
+        nodes.EnumerationFactor(
+            variables=(variable,),
+            configs=np.array([[1]]),
+            log_potentials=np.array([0.0, 1.0]),
+        )
 
-    with pytest.raises(ValueError) as verror:
-        nodes.EnumerationFactor(tuple([v]), configs, log_potentials)
-
-    assert "Potential" in str(verror.value)
-
-
-def test_enumfactor_configsshape_error():
-    v1 = nodes.Variable(3)
-    v2 = nodes.Variable(3)
-    configs = np.array([[1]], dtype=int)
-    log_potentials = np.array([1.0])
-
-    with pytest.raises(ValueError) as verror:
-        nodes.EnumerationFactor(tuple([v1, v2]), configs, log_potentials)
-
-    assert "Number of variables" in str(verror.value)
-
-
-def test_enumfactor_potentialshape_error():
-    v = nodes.Variable(3)
-    configs = np.array([[1]], dtype=int)
-    log_potentials = np.array([1.0, 2.0])
-
-    with pytest.raises(ValueError) as verror:
-        nodes.EnumerationFactor(tuple([v]), configs, log_potentials)
-
-    assert "The potential array has" in str(verror.value)
-
-
-def test_enumfactor_configvarsize_error():
-    v1 = nodes.Variable(3)
-    v2 = nodes.Variable(1)
-    configs = np.array([[-1, 4]], dtype=int)
-    log_potentials = np.array([1.0])
-
-    with pytest.raises(ValueError) as verror:
-        nodes.EnumerationFactor(tuple([v1, v2]), configs, log_potentials)
-
-    assert "Invalid configurations for given variables" in str(verror.value)
+    with pytest.raises(ValueError, match="Invalid configurations for given variables"):
+        nodes.EnumerationFactor(
+            variables=(variable,),
+            configs=np.array([[10]]),
+            log_potentials=np.array([0.0]),
+        )
