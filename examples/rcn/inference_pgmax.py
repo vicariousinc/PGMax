82d4c3f2a21ff5c91b2aa816ff8cae401c9d3cfe
Shrinu Kushagra <skushagra@ldp-skushagra.corp.vicarious.com>
2021-11-02 12:45:51 -0700

    initial commit after memory fixes

--- examples/rcn/inference_pgmax.py
+++ examples/rcn/inference_pgmax.py
@@ -1,21 +1,51 @@
 # export
+import gc
 import os
-
-os.environ["XLA_PYTHON_ALLOCATOR"] = "platform"
-os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+import time
 
 import matplotlib.pyplot as plt
 import numpy as np
 from learning import Model, index_to_rc
 from load_data import get_mnist_data_iters
 
+from pgmax.fg import graph, groups
+
+os.environ["XLA_PYTHON_ALLOCATOR"] = "platform"
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+
 debug = False
 
 
-import time
+def make_all_models(train_set, factor_type="pgmax"):
+    start = time.time()
+    hps, vps = 11, 11
+    models = []
 
-# export
-from pgmax.fg import graph, groups
+    for idx in range(len(train_set)):
+        img = train_set[idx][0]
+        models.append(
+            Model(img, hps, vps, max_cxn_length=100, factor_type=factor_type, alpha=1.0)
+        )
+
+        if idx % 10 == 0:
+            print(f"Done making {idx+1}th model.")
+
+    end = time.time()
+    print(f"Making models took {end-start:.3f} seconds.")
+    return models
+
+
+def reset_device_memory():
+
+    dvals = (x for x in gc.get_objects() if isinstance(x, jax.xla.DeviceArray))
+    n_deleted = 0
+    for dv in dvals:
+        dv.delete()
+        n_deleted += 1
+        del dv
+    del dvals
+    gc.collect()
+    return n_deleted
 
 
 def make_pgmax_graph(models):
@@ -49,35 +79,40 @@ def make_pgmax_graph(models):
     return fg
 
 
-def do_pgmax_inference(
-    test_img, fg, models, num_iters=10, damping_factor=0.5, verbose=False
-):
+def do_inference_on_batch(fg, models, test_set):
+    run_bp_fn, _, get_beliefs_fn = graph.BP(fg.bp_state, 30)
 
-    start = time.time()
+    scores = np.zeros((len(test_set), len(models)))
 
-    init_msgs = fg.get_init_msgs()
-    bu_msgs = [0] * len(models)
-    for model_idx, model in enumerate(models):
-        bu_msgs[model_idx] = model.initialize_vertex_beliefs(test_img)
+    for idx in range(len(test_set)):
+        img = test_set[idx][0]
 
-        unary_msgs = np.zeros((len(model.V), model.factors[0].M))
-        for i, v in enumerate(model.V):
-            unary_msgs[i, :] = v.get_initial_beliefs()
+        start = time.time()
+        bu_msgs = [0] * len(models)
 
-        init_msgs.evidence[model_idx] = unary_msgs
+        evidence_updates = {}
+        for model_idx in range(len(models)):
+            model = models[model_idx]
+            bu_msgs[model_idx] = model.initialize_vertex_beliefs(img)
 
-    end = time.time()
-    if verbose:
-        print(f"Initializing beliefs took {end-start:.3f} seconds.")
-    start = end
+            unary_msgs = np.zeros((len(model.V), model.factors[0].M))
+            for i, v in enumerate(model.V):
+                unary_msgs[i, :] = v.get_initial_beliefs()
 
-    msgs = fg.run_bp(num_iters, damping_factor, init_msgs)
-    map_states = fg.decode_map_states(msgs)
+            evidence_updates[model_idx] = unary_msgs
 
-    end = time.time()
-    if verbose:
-        print(f"Max product inference took {end-start:.3f} seconds.")
-    return map_states, bu_msgs
+        end = time.time()
+        # print(f"Initializing beliefs took {end-start:.3f} seconds.")
+        start = end
+
+        map_states = graph.decode_map_states(
+            get_beliefs_fn(run_bp_fn(evidence_updates=evidence_updates))
+        )
+        end = time.time()
+        # print(f"Max product inference took {end-start:.3f} seconds.")
+
+        scores[idx, :] = get_pgmax_scores(map_states, bu_msgs, models)
+    return scores
 
 
 def get_pgmax_scores(map_states, bu_msgs, models, verbose=False):
@@ -90,7 +125,7 @@ def get_pgmax_scores(map_states, bu_msgs, models, verbose=False):
         max_msg = -1 + np.zeros_like(bu_msgs[model_idx])
 
         for i, v in enumerate(model.V):
-            idx = map_states[(model_idx, i)]
+            idx = map_states[model_idx][i]
 
             f, r, c = v.get_frc()
             delta_r, delta_c = index_to_rc(idx, hps, vps)
@@ -107,68 +142,42 @@ def get_pgmax_scores(map_states, bu_msgs, models, verbose=False):
     return np.array(scores)
 
 
-# export
+def get_next_data_batch(batch_size):
+    i = 0
+    while i < len(train_set):
+        i_f = min(i + batch_size, len(train_set))
+        fg = make_pgmax_graph(models[i:i_f])
+        j = 0
+        while j < len(test_set):
+            j_f = min(j + batch_size, len(test_set))
+            yield fg, models[i:i_f], test_set[j:j_f], (i, i_f, j, j_f)
+            j += batch_size
+        i += batch_size
+
+
 data_dir = "/storage/users/skushagra/MNIST/"
 train_set, test_set = get_mnist_data_iters(data_dir, 20, 20)
 
-# export
-if debug:
-    img = train_set[5][0]
-    temp_model = Model(img, 11, 11, max_cxn_length=100, factor_type="pgmax", alpha=1.0)
-    plt.figure(figsize=(10, 10))
-    temp_model.visualize_graph()
-
-# export
-start = time.time()
-
-
-hps, vps = 11, 11
-models = []
 train_labels = -1 + np.zeros(len(train_set))
-
-for idx in range(len(train_set)):
-    img = train_set[idx][0]
-
-    train_labels[idx] = int(train_set[idx][1])
-    models.append(
-        Model(img, hps, vps, max_cxn_length=100, factor_type="pgmax", alpha=1.0)
-    )
-
-    if idx % 10 == 0:
-        print(f"Done making {idx+1}th model.")
-
-
-end = time.time()
-print(f"Making models took {end-start:.3f} seconds.")
-
-fg = make_pgmax_graph(models)
-
-# export
-import time
-
-from jax.interpreters import xla
-
-xla._xla_callable.cache_clear()
-
 test_labels = -1 + np.zeros(len(test_set))
+for i in range(len(test_set)):
+    train_labels[i] = test_set[i][1]
+for i in range(len(test_set)):
+    test_labels[i] = test_set[i][1]
+
+
+batch_size = 50
+models = make_all_models(train_set, factor_type="cpp")
 pgmax_scores = np.zeros((len(test_set), len(models)))
 
-start = time.time()
-for idx in range(len(test_set)):
 
-    img = test_set[idx][0]
-    test_labels[idx] = int(test_set[idx][1])
-
-    map_states, bu_msgs = do_pgmax_inference(img, fg, models, num_iters=10)
-    pgmax_scores[idx, :] = get_pgmax_scores(map_states, bu_msgs, models)
-
-    xla._xla_callable.cache_clear()
-    print(f"Done with test image {idx+1}")
-
-print(f"Making predictions on {idx+1} model took {time.time() - start} seconds.")
+for batch_fg, batch_models, batch_test_set, indices in get_next_data_batch(batch_size):
+    i, i_f, j, j_f = indices
+    scores = do_inference_on_batch(batch_fg, batch_models, batch_test_set)
+    pgmax_scores[j:j_f, i:i_f] = scores
+    print(f"Done with test images {j, j_f} on models {i, i_f}")
 
 
-# export
 test_preds = train_labels[pgmax_scores.argmax(axis=1)]
 accuracy = (test_preds == test_labels).sum() / test_labels.shape[0]
 
