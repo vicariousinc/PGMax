b39064d9b632441255a560652ff8e6df7e508cfb
stannis <stannis@vicarious.com>
2021-11-30 20:40:37 -0800

    Fix docs

--- pgmax/fg/graph.py
+++ pgmax/fg/graph.py
@@ -789,12 +789,12 @@ def BP(bp_state: BPState, num_iters: int) -> Tuple[Callable, Callable, Callable]
         num_iters: Number of belief propagation iterations.
 
     Returns:
-        Tuple containing:
-            run_bp: Function for running belief propagation for num_iters.
-                Optionally takes as input log_potentials updates, ftov_msgs updates,
-                evidence updates, and damping factor, and outputs a BPArrays.
-            get_bp_state: Function to reconstruct the BPState from BPArrays.
-            get_beliefs: Function to calculate beliefs from BPArrays.
+        Tuple containing\n
+        \trun_bp: Function for running belief propagation for num_iters.\n
+        \t\tOptionally takes as input log_potentials updates, ftov_msgs updates,
+        \t\tevidence updates, and damping factor, and outputs a BPArrays.\n
+        \tget_bp_state: Function to reconstruct the BPState from BPArrays.\n
+        \tget_beliefs: Function to calculate beliefs from BPArrays.\n
     """
     max_msg_size = int(np.max(bp_state.fg_state.wiring.edges_num_states))
     num_val_configs = (
