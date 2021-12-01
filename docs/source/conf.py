6235cd05e0d190ad634f26fc3d29ff8a25a34903
stannis <stannis@vicarious.com>
2021-12-01 13:31:31 -0800

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = (
 )
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
