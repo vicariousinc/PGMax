2be16e4b4df63097469d4f99467a968d2e287ceb
stannis <stannis@vicarious.com>
2022-02-18 16:01:03 -0800

    Bump up version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ copyright = "2021, Vicarious FPC Inc"
 author = "Guangyao Zhou, Nishanth Kumar, Miguel Lazaro-Gredilla, Shrinu Kushagra, Dileep George"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.2"
+release = "0.2.3"
 
 
 # -- General configuration ---------------------------------------------------
